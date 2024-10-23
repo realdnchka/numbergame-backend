4eba90744ee4860655ae46f53ddf591647529496
Danil Chernov <realdnchka@proton.me>
2024-10-23 11:34:16 +0700

    add new tests, change generator logic, update README.md

--- utils/number_generator.go
+++ utils/number_generator.go
@@ -8,42 +8,33 @@ func GenerateNumbers(totalNumbers int) ([]int, int) {
 	var numbers []int
 	sum := 0
 	
-	//Generates 'n' numbers
-	numbers = generateInitialNumbers(totalNumbers)
+	for sum < 10 {
+		//Generates 'n' numbers
+		for i:= 0; i < totalNumbers; i++ {
+			num := rand.IntN(50) + 1
+			numbers = append(numbers, num)
+		}
+		
+		//Generates random sum from numbers
+		for i:= 0; i < rand.IntN(totalNumbers - 2) + 2; i++ {
+			sum += numbers[i]
+		}
+		
+		if sum < 10 {
+			sum = 0
+			numbers = nil
+		}
+	}
 	
-	//Generates random sum from numbers
-	sum = generateSum(numbers, totalNumbers)
-	
-	//Remove one solution and numbers that greater than sum variants
+	//Remove one solution variants
 	for i:= 0; i < totalNumbers; i++ {
-		if sum <= numbers[i] {
-			numbers[i] = rand.IntN(sum) + 1
+		if sum == numbers[i] {
+			numbers[i] = rand.IntN(50) + 1
 			i = i - 1
 		}
 	}
 	
 	return shuffle(numbers), sum
-}
-
-func generateInitialNumbers(totalNumbers int) (numbers []int) {
-	for i:= 0; i < totalNumbers; i++ {
-		num := rand.IntN(50) + 1
-		numbers = append(numbers, num)
-	}
-	return numbers
-}
-
-func generateSum(numbers []int, totalNumbers int) (sum int) {
-	sum = 0
-	for i:= 0; i < rand.IntN(totalNumbers - 2) + 2; i++ {
-		sum += numbers[i]
-	}
-	
-	if sum < 10 {
-		sum = generateSum(generateInitialNumbers(totalNumbers), totalNumbers)
-	}
-	
-	return sum
 }
 
 //
