cc995b2f48a90bd1d617ef686c99df336f30baad
Danil Chernov <realdnchka@proton.me>
2024-10-23 12:56:42 +0700

    add auth from gcp secret manager

--- main.go
+++ main.go
@@ -15,12 +15,12 @@ func init() {
 }
 
 func main() {
-	http.HandleFunc("/getNumbers", utils.Logging(endpoints.GetNumbers))
-	http.HandleFunc("/createUser", utils.Logging(endpoints.CreateUser))
+	http.HandleFunc("/getNumbers", utils.Logging(utils.ApiKey(endpoints.GetNumbers)))
+	http.HandleFunc("/createUser", utils.Logging(utils.ApiKey(endpoints.CreateUser)))
 	
 	port := os.Getenv("PORT")
     if port == "" {
-            port = "80"
+        port = "80"
     }
 	http.ListenAndServe(":" + port, nil)
 }
