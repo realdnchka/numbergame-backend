8c0af82b6bd5c70a819fa9fed577af01a8697d63
Danil Chernov <realdnchka@proton.me>
2024-10-23 11:00:59 +0700

    some fixes

--- utils/number_generator.go
+++ utils/number_generator.go
@@ -9,25 +9,41 @@ func GenerateNumbers(totalNumbers int) ([]int, int) {
 	sum := 0
 	
 	//Generates 'n' numbers
-	for i:= 0; i < totalNumbers; i++ {
-		num := rand.IntN(50) + 1
-		numbers = append(numbers, num)
-	}
+	numbers = generateInitialNumbers(totalNumbers)
 	
 	//Generates random sum from numbers
-	for i:= 0; i < rand.IntN(totalNumbers - 2) + 2; i++ {
-		sum += numbers[i]
-	}
+	sum = generateSum(numbers, totalNumbers)
 	
-	//Remove one solution variants
+	//Remove one solution and numbers that greater than sum variants
 	for i:= 0; i < totalNumbers; i++ {
-		if sum == numbers[i] {
-			numbers[i] = rand.IntN(50) + 1
+		if sum <= numbers[i] {
+			numbers[i] = rand.IntN(sum) + 1
 			i = i - 1
 		}
 	}
 	
 	return shuffle(numbers), sum
+}
+
+func generateInitialNumbers(totalNumbers int) (numbers []int) {
+	for i:= 0; i < totalNumbers; i++ {
+		num := rand.IntN(50) + 1
+		numbers = append(numbers, num)
+	}
+	return numbers
+}
+
+func generateSum(numbers []int, totalNumbers int) (sum int) {
+	sum = 0
+	for i:= 0; i < rand.IntN(totalNumbers - 2) + 2; i++ {
+		sum += numbers[i]
+	}
+	
+	if sum < 10 {
+		sum = generateSum(generateInitialNumbers(totalNumbers), totalNumbers)
+	}
+	
+	return sum
 }
 
 //
