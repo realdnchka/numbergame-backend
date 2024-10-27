a97fa5c062550d550bbb00ca06d0d6e7b5433295
Danil Chernov <realdnchka@proton.me>
2024-10-27 12:04:49 +0700

    add leaderboard cache

--- endpoints/get_leaderboard.go
+++ endpoints/get_leaderboard.go
@@ -11,24 +11,31 @@ import (
 func GetLeaderboard(w http.ResponseWriter, r *http.Request) {
 	var leaderboard []utils.LeaderboardEntry
 	ctx := r.Context()
+	
+	cachedLeaderboard, found := utils.GetFromCache("leaderboard")
+	if found {
+		json.NewEncoder(w).Encode(cachedLeaderboard)
+		return
+	}
 
 	iter := utils.Client.Collection("users").OrderBy("highscore", firestore.Desc).Limit(10).Documents(ctx)
 	for {
-			doc, err := iter.Next()
-			if err == iterator.Done {
-				break
-			}
-			if err != nil {
-				http.Error(w, "Failed to retrieve documents", http.StatusInternalServerError)
-				return
-			}
-			var entry utils.LeaderboardEntry
-					if err := doc.DataTo(&entry); err != nil {
-						http.Error(w, "Failed to parse document", http.StatusInternalServerError)
-						return
-					}
-					leaderboard = append(leaderboard, entry)
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
 		}
+		if err != nil {
+			http.Error(w, "Failed to retrieve documents", http.StatusInternalServerError)
+			return
+		}
+		var entry utils.LeaderboardEntry
+		if err := doc.DataTo(&entry); err != nil {
+			http.Error(w, "Failed to parse document", http.StatusInternalServerError)
+			return
+		}
+		leaderboard = append(leaderboard, entry)
+	}
 	
+	utils.SetToCache("leaderboard", leaderboard)
 	json.NewEncoder(w).Encode(leaderboard)
 }
